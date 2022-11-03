71322748c709dd8d2fbabde37991b28027f6ce39
jay-2001 <90552695+jay-2001@users.noreply.github.com>
2022-11-03 19:26:51 +0530

    commit1

--- web_browser/settings.py
+++ web_browser/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$8rayx3=@x#^gfgs+8@(2^-gx*5j!%ock1i=1i=0f&8xl+rp0e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
